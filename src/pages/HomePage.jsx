74b56d88465edb9ba87978ffbfaedfe61ac41094
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-05-26 15:09:17 +0300

    Add theme: make default dark mode

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,9 +3,9 @@ import { Grid, GridItem } from "@chakra-ui/react";
 const HomePage = () => {
   return (
     <Grid h="100vh" templateColumns="repeat(5, 1fr)">
-      <GridItem colSpan={1} bg="tomato"></GridItem>
-      <GridItem colSpan={3} bg="papayawhip"></GridItem>
-      <GridItem colSpan={1} bg="tomato"></GridItem>
+      <GridItem colSpan={1}></GridItem>
+      <GridItem colSpan={3}></GridItem>
+      <GridItem colSpan={1}></GridItem>
     </Grid>
   );
 };
