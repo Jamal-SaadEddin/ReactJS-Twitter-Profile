7b14b7135dac1d069a56e43f18f7aa5e8e06ee19
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-05-31 20:57:42 +0300

    Add profile tabs & posts

--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -5,12 +5,21 @@ import {
   Icon,
   Image,
   Link,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
   VStack,
 } from "@chakra-ui/react";
-import { FaRegCalendarAlt } from "react-icons/fa";
+import { BiRepost } from "react-icons/bi";
+import { CiBookmark } from "react-icons/ci";
+import { FaRegCalendarAlt, FaRegComment } from "react-icons/fa";
 import { FaArrowLeftLong } from "react-icons/fa6";
+import { IoHeartOutline, IoShareOutline } from "react-icons/io5";
 import { LuBellPlus } from "react-icons/lu";
-import { MdMoreHoriz } from "react-icons/md";
+import { MdMoreHoriz, MdOutlineViewKanban } from "react-icons/md";
 import { RiLinkM } from "react-icons/ri";
 import PAGE_COVER_PICTURE from "../assets/React-cover-photo.png";
 import PAGE_PROFILE_PICTURE from "../assets/React-profile-photo.png";
@@ -125,6 +134,210 @@ const MainArea = () => {
           </Heading>
         </HStack>
       </VStack>
+      <Tabs isFitted>
+        <TabList>
+          <Tab fontWeight={700}>Posts</Tab>
+          <Tab fontWeight={700}>Replies</Tab>
+          <Tab fontWeight={700}>Media</Tab>
+          <Tab fontWeight={700}>Likes</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel p={0}>
+            <HStack
+              width="100%"
+              alignItems="start"
+              borderTop="2px solid"
+              borderTopColor="gray.600"
+              p={5}
+            >
+              <Image
+                src={PAGE_PROFILE_PICTURE}
+                width="40px"
+                borderRadius={100}
+              />
+              <VStack width="100%" alignItems="start" gap={0}>
+                <HStack gap={1}>
+                  <Heading fontSize="md">React</Heading>
+                  <Heading fontSize="md" color="gray.600" fontWeight={400}>
+                    @reactjs · Jan 18
+                  </Heading>
+                </HStack>
+                <Text fontSize="md">
+                  React Conf is back for 2024! Join our co-hosts @Meta and
+                  @callstackio May 15-16 in Henderson, Nevada or join the free
+                  livestream.
+                  <br />
+                  <br />
+                  Sign up here for updates: https://conf.react.dev
+                </Text>
+                <HStack width="100%" justifyContent="space-between" mt={1}>
+                  <Button
+                    leftIcon={<FaRegComment size="20px" />}
+                    fontSize="sm"
+                    background="none"
+                    color="gray.600"
+                    borderRadius={100}
+                    padding={0}
+                  >
+                    58
+                  </Button>
+                  <Button
+                    leftIcon={<BiRepost size="25px" />}
+                    fontSize="sm"
+                    background="none"
+                    color="gray.600"
+                    borderRadius={100}
+                    padding={0}
+                  >
+                    355
+                  </Button>
+                  <Button
+                    leftIcon={<IoHeartOutline size="20px" />}
+                    fontSize="sm"
+                    background="none"
+                    color="gray.600"
+                    borderRadius={100}
+                    padding={0}
+                  >
+                    1.5K
+                  </Button>
+                  <Button
+                    leftIcon={<MdOutlineViewKanban size="20px" />}
+                    fontSize="sm"
+                    background="none"
+                    color="gray.600"
+                    borderRadius={100}
+                    padding={0}
+                  >
+                    309K
+                  </Button>
+                  <HStack gap={0}>
+                    <Button
+                      fontSize={20}
+                      background="none"
+                      color="gray.600"
+                      borderRadius={100}
+                      padding={0}
+                    >
+                      <CiBookmark />
+                    </Button>
+                    <Button
+                      fontSize={20}
+                      background="none"
+                      color="gray.600"
+                      borderRadius={100}
+                      padding={0}
+                    >
+                      <IoShareOutline />
+                    </Button>
+                  </HStack>
+                </HStack>
+              </VStack>
+            </HStack>
+            <HStack
+              width="100%"
+              alignItems="start"
+              borderTop="2px solid"
+              borderTopColor="gray.600"
+              p={5}
+            >
+              <Image
+                src={PAGE_PROFILE_PICTURE}
+                width="40px"
+                borderRadius={100}
+              />
+              <VStack width="100%" alignItems="start" gap={0}>
+                <HStack gap={1}>
+                  <Heading fontSize="md">React</Heading>
+                  <Heading fontSize="md" color="gray.600" fontWeight={400}>
+                    @reactjs · Jan 18
+                  </Heading>
+                </HStack>
+                <Text fontSize="md">
+                  React Conf is back for 2024! Join our co-hosts @Meta and
+                  @callstackio May 15-16 in Henderson, Nevada or join the free
+                  livestream.
+                  <br />
+                  <br />
+                  Sign up here for updates: https://conf.react.dev
+                </Text>
+                <HStack width="100%" justifyContent="space-between" mt={1}>
+                  <Button
+                    leftIcon={<FaRegComment size="20px" />}
+                    fontSize="sm"
+                    background="none"
+                    color="gray.600"
+                    borderRadius={100}
+                    padding={0}
+                  >
+                    58
+                  </Button>
+                  <Button
+                    leftIcon={<BiRepost size="25px" />}
+                    fontSize="sm"
+                    background="none"
+                    color="gray.600"
+                    borderRadius={100}
+                    padding={0}
+                  >
+                    355
+                  </Button>
+                  <Button
+                    leftIcon={<IoHeartOutline size="20px" />}
+                    fontSize="sm"
+                    background="none"
+                    color="gray.600"
+                    borderRadius={100}
+                    padding={0}
+                  >
+                    1.5K
+                  </Button>
+                  <Button
+                    leftIcon={<MdOutlineViewKanban size="20px" />}
+                    fontSize="sm"
+                    background="none"
+                    color="gray.600"
+                    borderRadius={100}
+                    padding={0}
+                  >
+                    309K
+                  </Button>
+                  <HStack gap={0}>
+                    <Button
+                      fontSize={20}
+                      background="none"
+                      color="gray.600"
+                      borderRadius={100}
+                      padding={0}
+                    >
+                      <CiBookmark />
+                    </Button>
+                    <Button
+                      fontSize={20}
+                      background="none"
+                      color="gray.600"
+                      borderRadius={100}
+                      padding={0}
+                    >
+                      <IoShareOutline />
+                    </Button>
+                  </HStack>
+                </HStack>
+              </VStack>
+            </HStack>
+          </TabPanel>
+          <TabPanel>
+            <p>Replies!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Media!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Likes!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 };
