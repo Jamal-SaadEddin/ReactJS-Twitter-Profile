691f8d630ffbcc338214732717bb2d414832c018
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-05-31 14:41:13 +0300

    Add profile details

--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,7 +1,17 @@
-import { Button, HStack, Heading, Icon, Image, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Heading,
+  Icon,
+  Image,
+  Link,
+  VStack,
+} from "@chakra-ui/react";
+import { FaRegCalendarAlt } from "react-icons/fa";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { LuBellPlus } from "react-icons/lu";
 import { MdMoreHoriz } from "react-icons/md";
+import { RiLinkM } from "react-icons/ri";
 import PAGE_COVER_PICTURE from "../assets/React-cover-photo.png";
 import PAGE_PROFILE_PICTURE from "../assets/React-profile-photo.png";
 
@@ -40,7 +50,7 @@ const MainArea = () => {
             left="30px"
             alt="Profile Image"
           />
-          <HStack m={3} alignSelf="end">
+          <HStack mr={5} mb={3} alignSelf="end">
             <Button
               fontSize={28}
               background="none"
@@ -68,6 +78,51 @@ const MainArea = () => {
               Following
             </Button>
           </HStack>
+        </HStack>
+      </VStack>
+      <VStack gap={0} alignItems="start" m={5}>
+        <Heading fontSize="xx-large">React</Heading>
+        <Heading fontSize="md" color="gray.600">
+          @reactjs
+        </Heading>
+        <Heading fontSize="md" fontWeight={400} mt={4}>
+          The library for web and native user interfaces
+        </Heading>
+        <HStack gap={5} mt={2}>
+          <Link href="https://react.dev/" target="_blank" color="#1A8BD7">
+            <Icon
+              as={RiLinkM}
+              color="gray.600"
+              position="relative"
+              top="5px"
+              mr={1}
+              fontSize="20px"
+            />
+            react.dev
+          </Link>
+          <Heading color="gray.600" fontSize="md" fontWeight={400}>
+            <Icon as={FaRegCalendarAlt} position="relative" top="1px" mr={1} />
+            Joined July 2013
+          </Heading>
+        </HStack>
+        <HStack gap={4} mt={2}>
+          <HStack gap={1}>
+            <Heading fontSize="md">267</Heading>
+            <Heading fontSize="md" color="gray.600" fontWeight={400}>
+              Following
+            </Heading>
+          </HStack>
+          <HStack gap={1}>
+            <Heading fontSize="md">708.1K</Heading>
+            <Heading fontSize="md" color="gray.600" fontWeight={400}>
+              Followers
+            </Heading>
+          </HStack>
+        </HStack>
+        <HStack mt={4}>
+          <Heading fontSize="md" color="gray.600" fontWeight={400}>
+            Not followed by anyone you’re following
+          </Heading>
         </HStack>
       </VStack>
     </div>
