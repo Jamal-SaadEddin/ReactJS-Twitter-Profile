311f85b9aa9e0f7c12117e3189d703ec2e434120
Jamal SaadEddin <104212352+Jamal-SaadEddin@users.noreply.github.com>
2024-06-01 14:42:49 +0300

    Add "Search" & "You might like" boxes

---
+++ src/components/DiscoverArea.jsx
@@ -0,0 +1,149 @@
+import {
+  Button,
+  HStack,
+  Heading,
+  Icon,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Link,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+import SVELTE from "../assets/svelte.png";
+import VERCEL from "../assets/vercel.png";
+import VITE from "../assets/vite.jpg";
+
+const DiscoverArea = () => {
+  return (
+    <VStack py={5} pr={5} gap={4}>
+      <InputGroup size="md">
+        <InputLeftElement pointerEvents="none">
+          <Icon as={CiSearch} color="gray.500" fontSize="20px" />
+        </InputLeftElement>
+        <Input
+          type="text"
+          placeholder="Search"
+          bg="#202327"
+          border="0"
+          borderRadius={100}
+        />
+      </InputGroup>
+      <VStack
+        width="100%"
+        alignItems="start"
+        py={3}
+        px={4}
+        border="1px solid"
+        borderColor="gray.600"
+        borderRadius={20}
+        gap={5}
+      >
+        <Heading fontSize="xl">You might like</Heading>
+        <HStack width="100%" justifyContent="space-between">
+          <HStack>
+            <Image
+              src={SVELTE}
+              width="40px"
+              borderRadius={50}
+              cursor="pointer"
+            />
+            <VStack alignItems="start" gap={1}>
+              <Heading fontSize="md" cursor="pointer">
+                Svelte
+              </Heading>
+              <Heading
+                fontSize="md"
+                fontWeight="500"
+                color="gray"
+                cursor="pointer"
+              >
+                @sveltejs
+              </Heading>
+            </VStack>
+          </HStack>
+
+          <Button
+            color="black"
+            bg="white"
+            borderRadius={100}
+            _hover={{ bg: "gray.100" }}
+          >
+            Follow
+          </Button>
+        </HStack>
+        <HStack width="100%" justifyContent="space-between">
+          <HStack>
+            <Image src={VITE} width="40px" borderRadius={50} cursor="pointer" />
+            <VStack alignItems="start" gap={1}>
+              <Heading fontSize="md" cursor="pointer">
+                Vite
+              </Heading>
+              <Heading
+                fontSize="md"
+                fontWeight="500"
+                color="gray"
+                cursor="pointer"
+              >
+                @vite_js
+              </Heading>
+            </VStack>
+          </HStack>
+
+          <Button
+            color="black"
+            bg="white"
+            borderRadius={100}
+            _hover={{ bg: "gray.100" }}
+          >
+            Follow
+          </Button>
+        </HStack>
+        <HStack width="100%" justifyContent="space-between">
+          <HStack>
+            <Image
+              src={VERCEL}
+              width="40px"
+              borderRadius={50}
+              cursor="pointer"
+            />
+            <VStack alignItems="start" gap={1}>
+              <Heading fontSize="md" cursor="pointer">
+                Vercel
+              </Heading>
+              <Heading
+                fontSize="md"
+                fontWeight="500"
+                color="gray"
+                cursor="pointer"
+              >
+                @vercel
+              </Heading>
+            </VStack>
+          </HStack>
+
+          <Button
+            color="black"
+            bg="white"
+            borderRadius={100}
+            _hover={{ bg: "gray.100" }}
+          >
+            Follow
+          </Button>
+        </HStack>
+        <Link
+          href="/"
+          color="#1A8BD7"
+          width="100%"
+          _hover={{ textDecoration: "none" }}
+        >
+          Show more
+        </Link>
+      </VStack>
+    </VStack>
+  );
+};
+
+export default DiscoverArea;
